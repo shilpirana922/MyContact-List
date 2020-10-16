59143d3e70d5a2ee37907dec0a1efc1bfcf04bc3
shilpirana922 <ranashilpi922@gmail.com>
2020-10-16 10:57:14 +0530

    Updates

---
+++ precache-manifest.f0f835803066a999fe7cb201801fa8fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "760ecbc6071c157348b90b842ca18118",
+    "url": "/MyContact-List/index.html"
+  },
+  {
+    "revision": "b1cfcc268b84dc84ec42",
+    "url": "/MyContact-List/static/css/2.19e10c4f.chunk.css"
+  },
+  {
+    "revision": "378d66b1ee7a005e4a09",
+    "url": "/MyContact-List/static/css/main.603a0310.chunk.css"
+  },
+  {
+    "revision": "b1cfcc268b84dc84ec42",
+    "url": "/MyContact-List/static/js/2.9f23e842.chunk.js"
+  },
+  {
+    "revision": "77301219951e82722631bcc01796f6f6",
+    "url": "/MyContact-List/static/js/2.9f23e842.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "378d66b1ee7a005e4a09",
+    "url": "/MyContact-List/static/js/main.1ca1ed75.chunk.js"
+  },
+  {
+    "revision": "167bb46e7045f5857df7",
+    "url": "/MyContact-List/static/js/runtime-main.2b444bec.js"
+  }
+]);
